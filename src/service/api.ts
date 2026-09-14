4c46dfe754ea610924613e6573e65d7d99dd689b
togautamji03-coder <togautamji03@gmail.com>
2026-09-14 17:25:41 +0530

    Update api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "https://school-demo-backend.onrender.com/api";
+  "https://school-demo-backend-forked.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
