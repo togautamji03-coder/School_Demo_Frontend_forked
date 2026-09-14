0558bf7b392de8b52728fa3d7d734b1222451b15
togautamji03-coder <togautamji03@gmail.com>
2026-09-14 17:26:22 +0530

    Update apiSlice.ts

--- src/service/apiSlice.ts
+++ src/service/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const BASE_URL =
-  import.meta.env.VITE_API_URL || "https://school-demo-backend.onrender.com/api";
+  import.meta.env.VITE_API_URL || "https://school-demo-backend-forked.onrender.com/api";
 
 export const apiSlice = createApi({
   reducerPath: "api",
